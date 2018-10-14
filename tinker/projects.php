15e8e557f04ec7626a868b810017cd14f621323e
Dev Ashish <bdevashish20@gmail.com>
2018-10-14 13:55:12 +0530

    Update projects.php

--- tinker/projects.php
+++ tinker/projects.php
@@ -1,55 +1,46 @@
-<!-- To generate a list of employees in a particular project, for which attendance will be taken -->
+<!-- To generate a list of ongoing projects, for which attendance will be taken -->
 
 <!DOCTYPE html>
 	<html lang="en">
 		<head>
 			<meta charset="utf-8">
-			<title>Attendance | DSV</title>
+			<title>List Of Projects</title>
 			<link rel="stylesheet" type="text/css" href=""/>
 		</head>
 		<body>
-			<h1>Daily Employee Attendance</h1>
+			<h1>List of Ongoing Projects</h1>
 <?php
-	// Present day Date, Project code and Project name
-	$date = date_format(date_create(), "Y-m-d");
-	$p_code = $_GET['p_c'];
-	$p_name = $_GET['p_n'];
+	$db = mysqli_connect("localhost", "tester", "123456", "tinker_db");
 
-	echo "<h2>{$p_name}</h2>";
-	echo "<h3>{$date}</h3>";
+	$query = "SELECT p_code, name FROM project WHERE status = 1;";
+	
+	$result = mysqli_query($db, $query) or ("Result fucked");
 
-	$dbc = mysqli_connect("localhost", "tester", "123456", "tinker_db") or die("Error connecting to the database - 1");
-
-	$query = "SELECT employee.e_id, employee.name, employee.cont_name FROM employee INNER JOIN on_project ON employee.e_id = on_project.emp_id WHERE on_project.p_code = $p_code";
-
-	$result = mysqli_query($dbc, $query);
-	// Table Header
+	// The Table Header
 ?>
-		<form method="post" action="store.php">
 			<table border="1">
 				<thead>
 					<tr>
-						<th>Employee ID</th>
-						<th>Name</th>
-						<th>Contractor's Name</th>
-						<th>Present</th>
+						<th>Project Code</th>
+						<th>Project Name</th>
+						<th>Option</th>
 					</tr>
 				</thead>
+
 				<tbody>
-<?php
-	
-	while($row=mysqli_fetch_row($result)) {
-		echo "<tr><td>{$row[0]}</td><td>{$row[1]}</td><td>{$row[2]}</td>";
-		echo "<td><input type='checkbox' name='present[]' value='{$row[0]}' /></td></tr>";
+<?php					
+
+	while($row = mysqli_fetch_row($result)) {
+		echo '<tr>';
+		echo '<td>'.$row[0].'</td><td>'.$row[1].'</td>';
+		echo "<td><a href=\"atten.php?p_c={$row[0]}&p_n={$row[1]}\">Take Attendance</a></td>";
+		echo '</tr>';
 	}
-	mysqli_close($dbc);
 ?>
 				</tbody>
 			</table>
-			<input type="hidden" name="date" value="<?php echo $date;?>" />
-			<input type="hidden" name="p_c" value="<?php echo $p_code;?>" />
-			<input type="submit" value="Save attendance" name="submit" />
-		</form>	
-		<a href="all_absent.php?p_code=<?php echo $p_code; ?>&date=<?php echo $date; ?>">All Absent</a>
+<?php
+	mysqli_close($db);
+?>
 		</body>
 	</html>
