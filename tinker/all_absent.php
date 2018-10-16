5a59ef00beb51b8d10e8038c4e0aeafa11b63c32
Dev Ashish <bdevashish20@gmail.com>
2018-10-16 16:56:36 +0530

    Add files via upload

---
+++ tinker/all_absent.php
@@ -0,0 +1,17 @@
+<?php
+	if ( isset($_GET['p_code']) && isset($_GET['date'])) {
+		
+		// Generating date in proper format
+		$date = (int)implode("", explode("-", $_GET['date']));
+
+		$dbc = mysqli_connect('localhost', 'tester', '123456', 'tinker_db') or die(mysqli_error());
+
+		$query = "INSERT INTO all_absent VALUES ({$_GET['p_code']}, {$date})";
+
+		$result = mysqli_query($dbc, $query) or die(mysqli_error());
+
+		if($result) {
+			echo '<p>Attendance saved, take attendance for another <a href="projects.php">project</a></p>';
+		} 
+	}
+?>
