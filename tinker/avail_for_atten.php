3d2cfe37f2904ae8f93e7f51f0dfa40fe34c64ae
Dev Ashish <bdevashish20@gmail.com>
2018-10-19 11:39:36 +0530

    Add files via upload
    Both the files have been changed to improve the standard and quality of the code. Contrast to the name, avail_for_atten.php contains functions and modules required, for smooth functioning of projects_code.php

--- tinker/avail_for_atten.php
+++ tinker/avail_for_atten.php
@@ -21,6 +21,7 @@
 		}
 	}
 
+
 	/* Generates an array filled with the details of the projects which are available for taking attendance */
 	function avail_arr_gen($db_conn, $date) {
 		$query = "SELECT p_code, name FROM project WHERE status = 1";
@@ -38,6 +39,7 @@
 
 		return $available;
 	}
+
 
 	/* Generates a list of all the projects which are available for taking attendance, for users to see */
 	function pro_list_gen($pro_det_array) {
@@ -84,4 +86,87 @@
 <?php				
 	}
 
+
+	/* Generates an array with a list of projects for which the attendance is taken but the costs have not been added */
+	function avail_for_cost($db_conn, $date) {
+		// Attendance taken for projects for today's date
+		$pro_atten = "SELECT DISTINCT p_code FROM emp_atten WHERE dated = $date";
+
+		// Cost taken for projects for today's date
+		$pro_cost = "SELECT DISTINCT emp_atten.p_code FROM (( emp_atten INNER JOIN project ON emp_atten.p_code = project.p_code) INNER JOIN emp_cost ON emp_atten.p_code = emp_cost.p_code) WHERE emp_cost.dated = $date AND project.status = 1";
+
+		$pro_atten_res = mysqli_query($db_conn, $pro_atten) or die('Project Attendance: '.mysqli_error($db_conn));
+
+		$pro_cost_res = mysqli_query($db_conn, $pro_cost) or die('Project Cost: '.mysqli_error($db_conn));
+
+		$pro_atten_arr = array(); // Store pro_atten_res result
+
+		$pro_cost_arr = array(); // Store pro_cost_res result
+
+		// Loop to store pro_atten_res result
+		while($row=mysqli_fetch_row($pro_atten_res)) {
+			$pro_atten_arr[] = $row[0];
+		}
+
+		// Loop to store pro_cost_res result
+		while($row=mysqli_fetch_row($pro_cost_res)) {
+			$pro_cost_arr[] = $row[0];
+		}
+
+		// Array to store project code for adding costs
+		$pro_ca_arr = array();
+
+		if (sizeof($pro_cost_arr)==0) {
+			$pro_ca_arr = $pro_atten_arr;
+		} else {
+			for($i=0;$i<sizeof($pro_atten_arr);$i++) {
+				if(!is_int(array_search($pro_atten_arr[$i], $pro_cost_arr))) {
+					$pro_ca_arr[] = $pro_atten_arr[$i];
+				}
+			}		
+		}
+
+		return $pro_ca_arr;
+	}
+
+
+	/* Gets the project details: name from db, taking an array filled with p_codes as the parameter */
+	function pro_names($db_conn, $pro_code_array) {
+		$pi_arr = array();
+
+		foreach ($pro_code_array as $pro_code) {
+			$pro_info_query = "SELECT p_code, name FROM project WHERE p_code = $pro_code";
+
+			$pi_query_res = mysqli_query($db_conn, $pro_info_query) or die("pro_names functions: " . mysqli_error($db_conn));
+
+			$row = mysqli_fetch_row($pi_query_res);
+
+			$pi_arr[] = array($row[0], $row[1]);
+		}
+
+		return $pi_arr;
+	}
+
+	/* Generates the list of projects available for adding costs, for the user to see */
+	function cost_list_gen($db_conn, $p_code_arr) {
+
+		if(sizeof($p_code_arr)>0) {
+		
+			$pi_arr = pro_names($db_conn, $p_code_arr);
+
+			// Generating table header
+			$head_arr = array('Project Code', 'Project Name', 'Option');
+			table_head_gen($head_arr);
+
+			foreach ($pi_arr as $pi_arr_elem) {
+				echo "<tr><td>".$pi_arr_elem[0]."</td><td>".$pi_arr_elem[1]."</td><td><a href=\"cost_store.php?p_c={$pi_arr_elem[0]}&p_n={$pi_arr_elem[1]}\">Add Costs</a></td></tr>";
+			}
+?>
+				</tbody>
+			</table>
+<?php
+		} else {
+			echo '<p>No projects to add costs, take employee <a href="projects.php">attendance</a> or add <a href="projects_timing.php">employee timings</a>.</p>';
+		}
+	}
 ?>
